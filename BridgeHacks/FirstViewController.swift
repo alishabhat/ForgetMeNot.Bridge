ade7d8c26871022f643695dee9d850eee66b27a7
Amit Bhat <amit.bhat@berkeley.edu>
2020-07-19 11:18:08 -0700

    Initial Commit

---
+++ BridgeHacks/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BridgeHacks
+//
+//  Created by Amit Bhat on 7/19/20.
+//  Copyright © 2020 alisha bridgehacks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BridgeHacks/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BridgeHacks
+//
+//  Created by Amit Bhat on 7/19/20.
+//  Copyright © 2020 alisha bridgehacks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
